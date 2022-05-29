cc2b68fb6e1ab906bbc1a8e60c9cf7ae206570aa
jfzara <zarajeanfabrice@gmail.com>
2022-05-29 16:22:38 +0200

    added files

--- front/js/index.js
+++ front/js/index.js
@@ -1,55 +1,41 @@
 const getDataApi = async () => {
-  await fetch ('http://localhost:3000/api/products')
-  .then ((response) => {
-    console.log (response);   
-    const  allProducts = response.json(); 
-    console.log(allProducts);
-  allProducts.then((response) => {
-  const articles = response;
-  
-  for (let article in articles) {
-    let productCard = document.createElement("div");
-        document.querySelector(".items").appendChild(productCard);
-        productCard.classList.add("product_card");
-        console.log(articles[article]) ;
-
-    let productLink = document.createElement("a");
-        productCard.appendChild(productLink);
-        productLink.href = "product.html?id=${articles[article]._id}";
-        productLink.classList.add("product_link");
-
-    let productImageDiv = document.createElement("div");
-        productLink.appendChild(productImageDiv);
-        productImageDiv.classList.add("product__image");
-
-    let productImage = document.createElement("Image");
-        productImageDiv.appendChild(productImage);
-        productImage.src = articles[article].imageUrl;
-
-    let productInfosDiv = document.createElement("div");
-        productLink.appendChild(productInfosDiv);
-        productInfosDiv.classList.add("product__infos");
-
-    let productInfosTitle = document.createElement("div");
-        productInfosDiv.appendChild(productInfosTitle);
-        productInfosTitle.classList.add("product__infos__title");
-        productInfosTitle.innerHTML = articles[article].name;
-
-    let productInfoPrice = document.createElement("div");
-        productInfosDiv.appendChild(productInfoPrice);
-        productInfoPrice.classList.add("product__infos__price");  
-        
-        articles[article].price = articles[article].price / 100;
-        productInfoPrice.innerHTML = new Intl.NumberFormat("fr-FR", {
-          style: "currency",
-          currency: "EUR",
-        }).format(articles[article].price);    
-      }
+    await fetch ('http://localhost:3000/api/products')
+    .then ((response) => {
+      console.log (response);   
+      const  allProducts = response.json(); 
+      console.log(allProducts);
+    allProducts.then((response) => {
+    const articles = response;
     
-  })
-      
-  .catch((err) => console.log(err));
-  })
-}
 
-  getDataApi();
+    for (let article of articles) {
+        
+        console.log(articles[article]) ;
+       
+
+        let productCardAncre = document.createElement("a");
+        document.querySelector(".items").appendChild(productCardAncre);
+        productCardAncre.classList.add(".card_ancre");
+        productCardAncre.innerHTML += ` 
+        <a href="./product.html?${article._id}">
+          <article>
+                <img src="${article.imageUrl}" alt="${article.altTxt}">
+                <h3 class="productName">${article.name}</h3>
+                <p> ${article.price}€</p>
+                <p class="productDescription">${article.description}</p>
+          </article>
+        </a>
+        `;
+
+        };
+
+    })
+        
+    .catch((err) => console.log(err));
+    })
+  }
+  
+    getDataApi();
+
+
+    
---
+++ front/js/product.js
@@ -0,0 +1,90 @@
+/*rappel syntaxe pour nouvelleFonction = "(()=>{});" */
+
+let idFromUrl = window.location.search.split("?").join("");
+let id = idFromUrl;/*recuperer le id sans le ? */
+console.log(idFromUrl);
+console.log(id);
+console.log(`This is the id: ${idFromUrl}`);
+let productImage = document.getElementsByTagName("img");
+
+
+let productName =  document.getElementById("title");
+let productPrice= document.getElementById("price");
+let productDescription = document.getElementById("description") ;
+
+let product =[];
+
+
+
+
+const getProduct = async () => {
+    await fetch (`http://localhost:3000/api/products?${idFromUrl}`)
+    .then ((res) => {res.json()})
+    .then ((promise) => { 
+        product = promise;
+    })
+    
+  }
+const showProduct = async () => {
+  await getProduct()
+
+  .then(function () {
+    document.getElementsByClassName(".item").innerHTML =`
+    <article>
+          <div class="item__img">
+            <!-- <img src="${product.imageUrl}" alt="${product.altTxt}"> -->
+          </div>
+          <div class="item__content">
+
+            <div class="item__content__titlePrice">
+              <h1 id="title"><!-- ${product.name} --></h1>
+              <p>Prix : <span id="price"><!-- ${product.price}--></span>€</p>
+            </div>
+
+            <div class="item__content__description">
+              <p class="item__content__description__title">Description :</p>
+              <p id="description"><!-- ${product.description} --></p>
+            </div>
+
+            <div class="item__content__settings">
+              <div class="item__content__settings__color">
+                <label for="color-select">Choisir une couleur :</label>
+                <select name="color-select" id="colors">
+                    <option value="">--SVP, choisissez une couleur --</option>
+<!--                       <option value="vert">vert</option>
+                    <option value="blanc">blanc</option> -->
+                </select>
+              </div>
+
+              <div class="item__content__settings__quantity">
+                <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+                <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+              </div>
+            </div>
+
+            <div class="item__content__addButton">
+              <button id="addToCart">Ajouter au panier</button>
+            </div>
+
+          </div>
+        </article>
+    `;
+          
+    
+      /*productName.innerHTML = product.name;
+          productImage.src = product.imageUrl;
+          productAltTxt = product.altTxt;
+          productPrice = product.price;
+          productDescription.innerText = product.description;*/    
+      });
+  
+}
+  getProduct(); 
+
+
+
+
+
+
+
+
